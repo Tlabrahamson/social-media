21b8671042c27ad7233e05f8f4f45abc7d254498
Tim Abrahamson <tim@kinetic.com>
2020-07-31 16:43:00 -0500

    Added context to hold user state across app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Axios from "axios";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import UserContext from "./context/UserContext";
 
 import "./style.css";
 
 export default function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenResponse = await Axios.post(
+        "http://localhost:5000/users/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenResponse.data) {
+        const userResponse = await Axios.get("http://localhost:5000/users/", {
+          headers: { "x-auth-token": token }
+        });
+        setUserData({
+          token,
+          user: userResponse.data
+        });
+      }
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-        </Switch>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </UserContext.Provider>
       </BrowserRouter>
     </>
   );
